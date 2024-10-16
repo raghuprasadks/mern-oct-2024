b04025646a32d4d77fd8925a19459cee30f5d2e8
raghuprasadks <prasadraghuks@gmail.com>
2024-10-16 11:28:17 +0530

    fetch api

--- programs/react/reactbasics/src/components/FetchAPIDemo.js
+++ programs/react/reactbasics/src/components/FetchAPIDemo.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const FetchAPIDemo = () => {
+    const [todos, setTodos] = useState([])
 
     const getTodo = () => {
         fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then(response => response.json())
-        .then(json => console.log(json))
+        .then(res => res.json())
+       // .then(data => console.log(data))
+       .then(data=>setTodos(data))
         .catch(error => console.log(error))
     }
     
   return (
     <div>
         <h1>FetchAPIDemo</h1>
-    <button onClick={getTodo}>Get Todo</button>    
+    <button onClick={getTodo}>Get Todo</button>   
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Completed</th>
+            </tr>
+        </thead>
+        <tbody>
+            {todos.map((todo) => <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td>{todo.completed.toString()}</td>
+            </tr>)}
+        </tbody>
+        </table> 
     </div>
   )
 }
