beefcb7961a7671022f7ae1ee00ff64fdea2e796
raghuprasadks <prasadraghuks@gmail.com>
2024-10-17 12:23:24 +0530

    node

--- programs/nodedemo/5-express-product.js
+++ programs/nodedemo/5-express-product.js
@@ -2,27 +2,11 @@ const express = require('express')
 
 const app = express()
 const port = 3000
-
+// middleware
 app.use(express.json())
 
-let products = [
-    {
-        id: 1,
-        name: 'Laptop'
-    },
-    {
-        id: 2,
-        name: 'Mobile'
-    },
-    {
-        id: 3,
-        name: 'Tablet'
-    },
-    {
-        id: 4,
-        name: 'Smart Watch'
-    }
-]
+let products =  [] 
+
 
 
 app.get('/products', (req, res) => {
@@ -44,6 +28,21 @@ app.post('/products', (req, res) => {
     products.push(product)
     res.json(product)
 })
+
+app.put('/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = products.find(product => product.id === parseInt(id))
+    product.name = req.body.name
+    res.json(product)
+})
+
+app.delete('/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = products.find(product => product.id === parseInt(id))
+    const index = products.indexOf(product)
+    products.splice(index, 1)
+    res.json(product)
+})  
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
