d54780b37d2e997c567bc02f9aec3d3ffe716f9d
raghuprasadks <prasadraghuks@gmail.com>
2024-10-17 10:53:50 +0530

    node

---
+++ programs/nodedemo/4-express-gettingstarted.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    })
+
+app.get('/about', (req, res) => {
+    res.send('About Us')
+    })
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Us')
+    })
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${port}`)
+    })
